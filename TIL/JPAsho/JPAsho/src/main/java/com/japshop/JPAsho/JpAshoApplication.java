6287c4e09590a22cca59171992ef6994c48d9e8b
Kim mangi <aksrl25@mgail.com>
2022-08-03 09:05:13 +0900

    jpa 공부 프로젝트 추가

---
+++ TIL/JPAsho/JPAsho/src/main/java/com/japshop/JPAsho/JpAshoApplication.java
@@ -0,0 +1,13 @@
+package com.japshop.JPAsho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAshoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAshoApplication.class, args);
+	}
+
+}
---
+++ TIL/JPAsho/JPAsho/src/test/java/com/japshop/JPAsho/JpAshoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japshop.JPAsho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAshoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
